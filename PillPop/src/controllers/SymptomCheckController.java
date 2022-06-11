bea739ef8018e156b391aa6c687cac253fa72024
InsiderStudio <73741413+InsiderStudio@users.noreply.github.com>
2022-06-11 22:39:36 +0300

    stress intensifying

--- PillPop/src/controllers/SymptomCheckController.java
+++ PillPop/src/controllers/SymptomCheckController.java
@@ -7,12 +7,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 public class SymptomCheckController implements Initializable {
 
 	@FXML
 	private BorderPane paneBackground;
+	
+	@FXML
+	private ImageView imageIshihara;
 
 	@FXML
 	private Button buttonCorrect, buttonWrong, buttonNone;
@@ -21,20 +26,19 @@ public class SymptomCheckController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 
-		
-		 
+		 changeImage(imageIshihara,"/resources/images/5_2.png");
 		 
 
 	}
 	
 	@FXML
 	private void correctAnswer(ActionEvent event) {
-	
+		changeImage(imageIshihara,"/resources/images/74_21.png");
 	}
 	
 	@FXML
 	private void wrongAnswer(ActionEvent event) {
-	
+		changeImage(imageIshihara,"/resources/images/45_48.png");
 	}
 	
 	@FXML
@@ -42,6 +46,12 @@ public class SymptomCheckController implements Initializable {
 		moveButtons();
 	}
 	
+	private void changeImage(ImageView imgView,String adress)
+	{
+		Image tempImg = new Image(adress);
+		imgView.setImage(tempImg);
+		
+	}
 	private void moveButtons()
 	{
 		double xc = buttonCorrect.getLayoutX();
